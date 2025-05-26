7fa134aa000b5f0213fc3c5781da5aac461f7248
asasland <2239024812@qq.com>
2025-05-26 18:08:31 +0800

    propertyRebornCommit

--- src/main/java/com/formal/twilight/item/PropertyRegistry.java
+++ src/main/java/com/formal/twilight/item/PropertyRegistry.java
@@ -11,11 +11,19 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraft.item.ItemStack;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class PropertyRegistry {
-    @SubscribeEvent
+    @SubscribeEvent//物品属性覆盖
     public static void propertyOverrideRegistry(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
             ItemModelsProperties.register(
-                    ItemRegistry.magicIngot.get(), new ResourceLocation(Utils.MOD_ID, "size"), ( itemStack, clientWorld,  livingEntity) -> itemStack.getCount());
+                    ItemRegistry.newmagicIngot.get(),
+                    new ResourceLocation(Utils.MOD_ID, "sprinting"),
+                    (stack, world, entity) -> entity != null && entity.isSprinting() ? 1.0F : 0.0F
+            );
+            ItemModelsProperties.register(
+                    ItemRegistry.magicIngot.get(),
+                    new ResourceLocation(Utils.MOD_ID, "size"),
+                    (stack, world, entity) -> stack.getCount()
+            );
         });
     }
 }
